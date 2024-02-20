ec0eb68d982acf6549514066ae9802971094ee42
Yixiao <1411101569@qq.com>
2024-02-20 07:04:43 +0000

    fix union error

--- src/ThumbnailCanvas.js
+++ src/ThumbnailCanvas.js
@@ -1,5 +1,5 @@
-import {useEffect, useRef} from "react";
-import ClipperLib from "clipper-lib"
+import React, {useEffect, useRef, useState} from "react";
+import ClipperLib from "clipper-lib";
 
 const ThumbnailCanvas = ({width, height, rectangles}) => {
     const canvasRef = useRef(null);
@@ -12,38 +12,67 @@ const ThumbnailCanvas = ({width, height, rectangles}) => {
 
         const clipper = new ClipperLib.Clipper();
 
-        rectangles.forEach(rect => {
+        const pathColors = []; // 存储每个路径对应的颜色
+
+
+        rectangles.forEach((rect, index) => {
             const rectPath = [
                 {X: rect.x, Y: rect.y},
                 {X: rect.x + rect.width, Y: rect.y},
                 {X: rect.x + rect.width, Y: rect.y + rect.height},
                 {X: rect.x, Y: rect.y + rect.height}
             ];
-            clipper.AddPath(rectPath, ClipperLib.PolyType.ptSubject, true);
+            console.log(rectPath);
+            clipper.AddPath(rectPath, ClipperLib.PolyType.ptClip, true);
+            pathColors.push(getRandomColor()); // 为每个路径生成一个随机颜色
         });
+
 
         const solution = new ClipperLib.Paths();
-        clipper.Execute(ClipperLib.ClipType.ctUnion, solution);
+        clipper.Execute(ClipperLib.ClipType.ctUnion, solution, ClipperLib.PolyFillType.pftNonZero, ClipperLib.PolyFillType.pftNonZero);
 
-        // 随机生成颜色
-        const randomColor = () => {
-            return '#' + Math.floor(Math.random() * 16777215).toString(16);
-        };
-
-        solution.forEach((path, index) => {
-            const fillColor = randomColor();
-            ctx.fillStyle = fillColor;
-            ctx.beginPath();
-            ctx.moveTo(path[0].X * (canvas.width / width), path[0].Y * (canvas.height / height));
-            path.slice(1).forEach(p => {
-                ctx.lineTo(p.X * (canvas.width / width), p.Y * (canvas.height / height));
-            });
+        // console.log(rectangles);
+        // console.log(solution);
+        ctx.fillStyle = "red";
+        ctx.strokeStyle = "black";
+        ctx.beginPath();
+        for (let i2 = 0; i2 < solution.length; i2++) {
+            for (let j = 0; j < solution[i2].length; j++) {
+                let x = solution[i2][j].X * (canvas.width / width);
+                let y = solution[i2][j].Y * (canvas.height / height);
+                if (!j) ctx.moveTo(x, y);
+                else ctx.lineTo(x, y);
+            }
             ctx.closePath();
-            ctx.fill();
-        });
+        }
+        ctx.fill();
+        ctx.stroke();
+
+        // solution.forEach((path, index) => {
+        //     ctx.beginPath();
+        //     ctx.moveTo(path[0].X * (canvas.width / width), path[0].Y * (canvas.height / height));
+        //     path.slice(1).forEach(p => {
+        //         ctx.lineTo(p.X * (canvas.width / width), p.Y * (canvas.height / height));
+        //     });
+        //     ctx.closePath();
+        //     ctx.fillStyle = pathColors[index]; // 为每个路径设置不同的颜色
+        //     ctx.fill();
+        //     ctx.stroke();
+        // });
+
+
     }, [width, height, rectangles]);
 
-    return <canvas ref={canvasRef} width={400} height={300} style={{border: '1px solid black'}}/>;
+    const getRandomColor = () => {
+        const letters = "0123456789ABCDEF";
+        let color = "#";
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    };
+
+    return <canvas ref={canvasRef} width={400} height={300} style={{border: '5px solid black'}}/>;
 };
 
 export default ThumbnailCanvas;
