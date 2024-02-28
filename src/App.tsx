8ae87994b23cb88dc9c52d2e533d9e5956102467
Yixiao <1411101569@qq.com>
2024-02-28 07:38:49 +0000

    feat: replace navbar with tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import './App.css';
 import Online2DDrawer from "./Online2DDrawer";
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-} from "react-router-dom";
-import {Container} from "react-bootstrap";
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 function App() {
 
-    function Home() {
+    function Editor() {
         return (
             <div className="App">
                 <Online2DDrawer/>
             </div>
+        );
+    }
+
+    function Manual() {
+        return (
+            <></>
         );
     }
 
@@ -30,38 +30,22 @@ function App() {
         );
     }
 
-    function NotFound() {
-        return (
-            <div>
-                <h2>404 Not Found</h2>
-                <p>Sorry, the page you are looking for does not exist.</p>
-            </div>
-        );
-    }
 
     return (
-        <Router>
-            <Navbar expand="lg" className="bg-body-tertiary">
-                <Container>
-                    <Navbar.Brand href="Home">Indoor Navigation Editor</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="About">About</Nav.Link>
-                            <Nav.Link href="https://github.com/webxr-indoor-navigation/Editor">GitHub</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-
-
-            <Routes>
-                <Route index element={<Home/>}/>
-                <Route path="/About" element={<About/>}/>
-                <Route path="/Home" element={<Home/>}/>
-                <Route path="*" element={<NotFound/>}/>
-            </Routes>
-        </Router>
+        <Tabs
+            defaultActiveKey="Editor"
+            className="mb-3"
+        >
+            <Tab eventKey="Editor" title="Editor">
+                <Editor></Editor>
+            </Tab>
+            <Tab eventKey="Manual" title="Manual" disabled>
+                <Manual></Manual>
+            </Tab>
+            <Tab eventKey="About" title="About" disabled>
+                <About></About>
+            </Tab>
+        </Tabs>
     );
 }
 
